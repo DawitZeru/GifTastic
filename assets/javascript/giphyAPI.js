b36770ba18f5b9fa53a1b78b86f9ba401c31223f
DawitZeru <47404177+DawitZeru@users.noreply.github.com>
2019-07-15 00:47:54 -0400

    last commit  giphy

--- assets/javascript/giphyAPI.js
+++ assets/javascript/giphyAPI.js
@@ -3,11 +3,11 @@ var topics = ["hangry", "excited", "lazy", "wild", "outraged",];
 
 $(document).ready(function() {
 
-    // calls the Gphy API
-    function gifGifs() {
+    // calls the Giphy API
+    function getGifs() {
         var searchName = $(this).attr(data-name);
         if (searchName === undefined) {
-            searchName = searchResults;
+            searchName = searchResult;
         }
     }
 })
@@ -20,6 +20,7 @@ $.ajax({
     method: "GET"
 }).then(function (response) {
     
+    // store data inresults variable and loop through
     var results = response.data;
     for (var i = 0; i < results.length; i++) {
 
@@ -34,5 +35,63 @@ $.ajax({
         let stillURL = results[i].images.fixed_width_still.url;
         newGif.attr("data-animate", gifUrl);
         newGif.addClass("gif");
+
+        $("#giphys").append(newGif);
+        resultDiv.append(newGif);
+        resultDiv.append(p);
+
+        $("#giphys").prepend(resultDiv);
     }
 });
+
+$("gif").on("click", function () {
+    let state = $(this).attr("date-animate");
+    if (state === 'still') {
+        const url = $(this).attr("date-animate");
+        $(this).attr("src", url);
+        $(this).attr("date-state", 'animate');
+        console.log(state);
+    }
+    else if (state === 'animate') {
+        const url = $(this).attr("date-still");
+        $(this).attr("src", url);
+        $(this).attr("date-state", 'still');
+        console.log(state); 
+    }
+});
+
+//shows gif data
+function displayButtons() {
+
+    $("#addTopics").empty();
+for (var i = 0; i < topics.length; i++) {
+   
+    //generates buttons for each topic in the array
+    var b = $("<buttons>");
+   // adds class of mood
+    b.addClass("mood btn btn-outline-primary")
+    // added a data attribute
+    b.attr("data-name", topics[i]);
+    // initial button text
+    b.text(topics[i]) ;
+    // added the button to the addTopics div
+    $("#addTopics").append(b);
+}
+}
+
+$("#searchButton").on("click", function (event) {
+    event.preventDefault();
+
+    searchResult = $("#searchInput").val().trim();
+    console.log("input: " + searchResult);
+
+    topics.push(searchResult);
+    console.log(topics);
+
+    getGifs(searchResult);
+    displayButtons();
+});
+
+$(document).on("click: ", "mood", getGifs);
+
+displayButtons();
